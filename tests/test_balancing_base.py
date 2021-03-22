7f798f9e09d261ff4931fca626c78ac653c52f80
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2021-03-22 13:40:12 +0300

    HH-125737 add consul based http client

--- tests/test_balancing_base.py
+++ tests/test_balancing_base.py
@@ -1,8 +1,10 @@
+from typing import Dict
+
 from tornado.httpclient import AsyncHTTPClient
 from tornado.testing import AsyncHTTPTestCase
 from tornado.web import RequestHandler, Application
 
-from http_client import HttpClientFactory, Server
+from http_client import HttpClientFactory, Server, Upstream, UpstreamStore
 
 
 class WorkingHandler(RequestHandler):
@@ -26,12 +28,27 @@ class BalancingClientMixin:
     def setUp(self):
         AsyncHTTPClient.configure('tornado.curl_httpclient.CurlAsyncHTTPClient')
         super().setUp()
-        self.http_client_factory = HttpClientFactory('testapp', self.http_client, {})
+        self.upstreams = {}
+        self.http_client_factory = HttpClientFactory('testapp', self.http_client,
+                                                     upstream_store=UpstreamStoreTestImpl(self.upstreams)
+                                                     )
         self.balancing_client = self.http_client_factory.get_http_client()
 
     def get_upstream_config(self):
         return {'max_fails': 10, 'fail_timeout_sec': 0.1, 'request_timeout_sec': 0.5}
 
     def register_ports_for_upstream(self, *ports):
-        self.http_client_factory.register_upstream('test', self.get_upstream_config(),
-                                                   [Server(f'127.0.0.1:{port}') for port in ports])
+        upstream = Upstream('test', self.get_upstream_config(),
+                            [Server(f'127.0.0.1:{port}') for port in ports])
+        return self.http_client_factory.upstream_store.update('test', upstream)
+
+
+class UpstreamStoreTestImpl(UpstreamStore):
+    def __init__(self, upstreams):
+        self.upstreams = upstreams
+
+    def get_upstream(self, host) -> Upstream:
+        return self.upstreams[host]
+
+    def update(self, host, upstream):
+        self.upstreams[host] = upstream
---
+++ tests/test_parser.py
@@ -0,0 +1,139 @@
+import json
+import unittest
+
+import tornado
+
+from http_client.options import options
+from http_client.consul_parser import parse_consul_upstream_config, parse_consul_health_servers_data
+
+
+class TestParser(unittest.TestCase):
+
+    def test_parse_config(self):
+        value = {'Value': json.dumps(
+            {
+                'hosts': {
+                    'default': {
+                        'profiles': {
+                            'default': {
+                                'max_tries': '3',
+                                'fail_timeout_sec': '1',
+                                'request_timeout_sec': '5',
+                                'connect_timeout_sec': '0.2',
+                                'max_fails': '5',
+                                'max_timeout_tries': '1'
+                            }
+                        }
+                    }
+                }
+            }
+        )}
+        config = parse_consul_upstream_config(value)
+
+        self.assertEqual(config['max_tries'], '3')
+        self.assertEqual(config['fail_timeout_sec'], '1')
+        self.assertEqual(config['request_timeout_sec'], '5')
+        self.assertEqual(config['connect_timeout_sec'], '0.2')
+        self.assertEqual(config['max_fails'], '5')
+        self.assertEqual(config['max_timeout_tries'], '1')
+
+    def test_parse_config_default_value(self):
+        value = {'Value': json.dumps(
+            {
+                'hosts': {
+                    'default': {
+                        'profiles': {
+                            'default': {
+                                'max_tries': '3'
+                            }
+                        }
+                    }
+                }
+            }
+        )}
+        config = parse_consul_upstream_config(value)
+
+        self.assertEqual(config['max_tries'], '3')
+        self.assertEqual(config['fail_timeout_sec'], options.http_client_default_fail_timeout_sec)
+
+    def test_parse_health_service(self):
+        value = [
+            {
+                'Node': {
+                    'ID': '1',
+                    'Node': '',
+                    'Address': '1.1.1.1',
+                    'Datacenter': 'test',
+                },
+                'Service': {
+                    'ID': '2',
+                    'Service': 'app',
+                    'Address': '',
+                    'Port': 9999,
+                    'Weights': {
+                        'Passing': 100,
+                        'Warning': 0
+                    }
+                }
+            }
+        ]
+
+        dc, servers = parse_consul_health_servers_data(value)
+
+        self.assertEqual(len(servers), 1)
+        self.assertEqual(servers[0].address, '1.1.1.1:9999')
+        self.assertEqual(servers[0].weight, 100)
+        self.assertEqual(servers[0].datacenter, 'test')
+
+    def test_parse_health_service_not_test_datacenter(self):
+        value = [
+            {
+                'Node': {
+                    'ID': '1',
+                    'Node': 'some_name',
+                    'Address': '1.1.1.1',
+                    'Datacenter': 'test',
+                },
+                'Service': {
+                    'ID': '2',
+                    'Service': 'app',
+                    'Address': '',
+                    'Port': 9999,
+                    'Weights': {
+                        'Passing': 100,
+                        'Warning': 0
+                    }
+                }
+            }
+        ]
+
+        dc, servers = parse_consul_health_servers_data(value)
+
+        self.assertEqual(len(servers), 1)
+
+    def test_parse_health_service_not_test_datacenter_with_self_enabled_filter(self):
+        tornado.options.options.self_node_filter_enabled = True
+        value = [
+            {
+                'Node': {
+                    'ID': '1',
+                    'Node': 'some_name',
+                    'Address': '1.1.1.1',
+                    'Datacenter': 'test',
+                },
+                'Service': {
+                    'ID': '2',
+                    'Service': 'app',
+                    'Address': '',
+                    'Port': 9999,
+                    'Weights': {
+                        'Passing': 100,
+                        'Warning': 0
+                    }
+                }
+            }
+        ]
+
+        dc, servers = parse_consul_health_servers_data(value)
+
+        self.assertEqual(len(servers), 0)
