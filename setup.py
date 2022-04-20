6d40842810d285761d655544e1c1315f752be30f
Pavel Orlov <p.orlov@hh.ru>
2022-04-20 16:41:06 +0300

    release 1.1.12

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class TestHook(test):
 
 setup(
     name='balancing-http-client',
-    version='1.1.11',
+    version='1.1.12',
     description='Balancing http client for tornado',
     url='https://github.com/hhru/balancing-http-client',
     cmdclass={
